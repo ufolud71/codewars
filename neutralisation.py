a7b8b57edddcc8e6a318706004c956ca1c80001e
ufolud71 <chudy.orfeusz@gmail.com>
2024-10-13 21:00:17 +0200

    Neutralisation Comment

--- neutralisation.py
+++ neutralisation.py
@@ -1,3 +1,8 @@
+#Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+#When positives and positives interact, they remain positive.
+#When negatives and negatives interact, they remain negative.
+#But when negatives and positives interact, they become neutral, and are shown as the number 0.
 def neutralise(s1, s2):
     stringList = []
     for i in range(len(s1)):
